ecec6f90112bd65520f643a788efdde806027031
Ana Cruz <acruz@itba.edu.ar>
2022-03-14 13:23:53 -0300

    VDS iterates to find best depth

--- TP1/algorithms/vds.py
+++ TP1/algorithms/vds.py
@@ -4,6 +4,8 @@ from classes.state import State
 from classes.node import Node
 from classes.board import Board
 
+import math
+
 
 
 
@@ -18,18 +20,13 @@ def find_solution(init_state: State, max_depth):
     expanded_count = 0
 
     while len(frontier)>0:
-        print("entering big while")
         current_node: Node = frontier.pop()
         while current_node.depth >= max_depth:
-            print("current node exceeded max depth")
             if len(frontier) == 0:
-                print("No possible solution with current")
-                return 
+                print("No possible solution with current depth")
+                return None, -1
             current_node = frontier.pop()
 
-        print(current_node.state.board)
-        print("node depth: ", current_node.depth)
-        print("expanded count: ", expanded_count)
         current_state: State = current_node.state
         current_board = current_state.board
         current_empty_space = current_state.empty_space
@@ -39,21 +36,21 @@ def find_solution(init_state: State, max_depth):
 
 
         if (explored_value == None) or (explored_value != None and explored_value[1] > current_node.depth):
-            print("Unexplored territory!")
-            if explored_value != None and explored_value[1] > current_node.depth:
-                print("previous depth: ", explored_value[1], " current depth: ", current_node.depth)
 
             explored_value = (current_node, current_node.depth) #the value in the dictionary is the lowest (in this case only) depth at witch this node was explored
             explored[board_string] = explored_value
 
             #Is this node the solution? 
             if current_node.state.is_final():
-                print("Final solution!")
+                print("Solution found!")
+                print(current_node.state.board)
+                final_depth = current_node.depth
+                print("Profundidad: ", final_depth, "Con profundidad maxima: ", max_depth)
                 solution = deque()
                 while current_node is not None:
                     solution.append(current_node)
                     current_node = current_node.parent
-                return solution, current_node.depth
+                return solution, final_depth
             #Go on to expand current node
             expanded_count +=1
             for movement in current_node.state.empty_space.get_movements(3,3):
@@ -66,11 +63,44 @@ def find_solution(init_state: State, max_depth):
 
     
     print("we're out!")
-    return None
+    return None, -1
 
 def run(init_state : State, initial_depth):
-    previous_depth = initial_depth
+    previous_depth = 0
+    new_depth = initial_depth
     solution, depth = find_solution(init_state, initial_depth)
+    
+    #busco la primer cota superior
+    while depth == -1:
+        previous_depth = new_depth
+        new_depth = previous_depth+10
+        print("about to try with depth: ", new_depth)
+        solution, depth = find_solution(init_state, new_depth)
+
+    lower_bound = previous_depth #ultima profundidad a la que no consegui solucion
+    upper_bound = depth # primer profundidad a la que consegui solucion
+ 
+
+    print("Tengo cota inferior: ", lower_bound, " y cota superior: ", upper_bound)
+
+    
+    while upper_bound - lower_bound > 1:
+        mid_point = math.trunc((lower_bound+upper_bound)/2)
+        print("about to try with depth: ", mid_point)
+        solution, depth = find_solution(init_state, mid_point)
+        if depth == -1: #midpoint doesnt find solutions, midpoint becomes new lower bound
+            if lower_bound == mid_point: #last case where lower_bound and upper bound are split by one number
+                return solution
+            lower_bound = mid_point
+        else: #midpoint finds solution, found depthbecomes new upper bound
+            upper_bound = depth
+    print("La solucion se obtiene de manera optima con la profundidad ", upper_bound)
+    return solution
+
+            
+
+
+
     
     
 
