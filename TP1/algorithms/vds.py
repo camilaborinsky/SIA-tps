204a6faaca3bd24a0c44f3b17b5958377c478722
Ana Cruz <acruz@itba.edu.ar>
2022-03-14 11:31:44 -0300

    vds con dict

--- TP1/algorithms/vds.py
+++ TP1/algorithms/vds.py
@@ -7,10 +7,10 @@ from classes.board import Board
 
 
 
-def run(init_state: State, max_depth):
+def find_solution(init_state: State, max_depth):
     initial_node = Node(None, init_state)
 
-    explored = deque()
+    explored = dict()
     initial_node.depth = 0;
     frontier = deque()
     frontier.append(initial_node)
@@ -19,45 +19,63 @@ def run(init_state: State, max_depth):
 
     while len(frontier)>0:
         print("entering big while")
-        current_node: Node = frontier.popleft()
+        current_node: Node = frontier.pop()
         while current_node.depth >= max_depth:
-            print("current node exceeded depth")
+            print("current node exceeded max depth")
             if len(frontier) == 0:
-                print("No possible solution with current depth")
+                print("No possible solution with current")
                 return 
-            current_node = frontier.popleft()
+            current_node = frontier.pop()
 
         print(current_node.state.board)
+        print("node depth: ", current_node.depth)
         print("expanded count: ", expanded_count)
         current_state: State = current_node.state
         current_board = current_state.board
         current_empty_space = current_state.empty_space
-        print(current_board)
-        if current_node not in explored:
+    
+        board_string = str(current_board.positions)    
+        explored_value = explored.get(board_string)
+
+
+        if (explored_value == None) or (explored_value != None and explored_value[1] > current_node.depth):
             print("Unexplored territory!")
-            explored.append(current_node)
+            if explored_value != None and explored_value[1] > current_node.depth:
+                print("previous depth: ", explored_value[1], " current depth: ", current_node.depth)
+
+            explored_value = (current_node, current_node.depth) #the value in the dictionary is the lowest (in this case only) depth at witch this node was explored
+            explored[board_string] = explored_value
+
             #Is this node the solution? 
             if current_node.state.is_final():
                 print("Final solution!")
                 solution = deque()
                 while current_node is not None:
-                    solution.appendleft(current_node)
+                    solution.append(current_node)
                     current_node = current_node.parent
-                return solution
+                return solution, current_node.depth
             #Go on to expand current node
             expanded_count +=1
             for movement in current_node.state.empty_space.get_movements(3,3):
-                print(movement)
-                print("hello")
                 new_board, new_empty = current_board.move(movement, current_empty_space)
                 new_node = Node(current_node, State(new_board, new_empty))
                 #Creo que esta linea es innecesaria por como esta definido depth en Node. 
                 new_node.depth = current_node.depth+1
-                print("new node depth: ", new_node.depth)
                 if not new_node in frontier:
                     frontier.append(new_node)
 
+    
     print("we're out!")
+    return None
+
+def run(init_state : State, initial_depth):
+    previous_depth = initial_depth
+    solution, depth = find_solution(init_state, initial_depth)
+    
+    
+
+
+
 
 
             
