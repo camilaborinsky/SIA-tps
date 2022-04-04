6da4cb1c9af50fe2de07b6030e9ba401134a43fb
Ana Cruz <acruz@itba.edu.ar>
2022-04-04 17:53:59 -0300

    Diversity calculations by fitness or individuals

--- TP2/classes/generation_metrics.py
+++ TP2/classes/generation_metrics.py
@@ -21,9 +21,33 @@ def max_fitness(population):
     return max(list(map(lambda individual: individual.fitness, population)))
 
 
+def invididuals_diversity(population):
+    #Create set with genotypes of all individuals
+    genotypes = set()
+    for individual in population:
+        genotypes.add(individual)
+    total_genotypes = len(genotypes)
+    return total_genotypes/len(population)
 
-def fitness_diversity(population):
-    fitness_unique_values = 0
-    # for i in enumerate(population):
-    #     for j in enumerate(population):
-    #         if(abs(i.fitness - j.fitness) > PRECISION ):
+def exact_fitness_diversity(population):
+    #Create set with fitnesses of all individuals
+    fitnesses = set()
+    for individual in population:
+        fitnesses.add(individual.fitness)
+    total_fitnesses = len(fitnesses)
+    return total_fitnesses/len(population)
+
+def fitness_diversity_with_precision(population):
+    #Chequeo todos los individuos del i hacia adelante a ver si alguno es "igual" que el current, si no hay ninguno incremento diversity. 
+    length = len(population)
+    diversity = 0
+    for i in range(0, length):
+        matched = False
+        for j in range (i+1, length):
+            if abs(population[i].fitness - population[j].fitness) > 0.00001: #FIXME change value to precision from input
+                matched = True
+        if not matched:
+            diversity += 1
+    return diversity/len(population)
+
+
