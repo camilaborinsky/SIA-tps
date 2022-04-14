949a6e7a70f5c8a639f83a68a510812fc3522e6a
camilaborinsky <cborinsky@itba.edu.ar>
2022-04-14 11:44:01 -0300

    remove w0 (temporarily)

--- TP3/perceptrons/perceptron.py
+++ TP3/perceptrons/perceptron.py
@@ -11,14 +11,14 @@ class Perceptron:
         self.learn_rate = learn_rate
     
     def learn(self, iteration_limit):
-        i = 0
+        i = 0 #nro de iteracion
         p = len(self.training_set)
-        w = random.uniform(-1, 1, len(self.training_set[0])+1)
+        w = random.uniform(-1, 1, len(self.training_set[0]))
         error = 1
         error_min = 2*p
         while error > 0 and i < iteration_limit:
             idx = random.randint(0, p)
-            h = dot(self.training_set[idx],w , out=None)
+            h = dot(self.training_set[idx],w, out=None)
             o = self.activation(h)
             w_diff = self.learn_rate*(self.expected_output[idx]-o)*self.training_set[idx]
             w += w_diff
