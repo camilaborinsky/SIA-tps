8db9f12e85ce79329cf9750829b9cbff6675ce57
Ana Cruz <acruz@itba.edu.ar>
2022-05-07 18:41:39 -0300

    Fixed parsing for expected output in ex 3.3

--- TP3/ex_3/main.py
+++ TP3/ex_3/main.py
@@ -44,12 +44,13 @@ def parse_ej3(file_path):
         with open(file_path+"/expected_ej3.txt") as f:
             number = []
             for line in f:
-                for item in line.replace("\n", "").split(","):
+                number = []
+                for item in line.split(","):
                     if(item != ""):
                         number.append(int(item))
                 expected_output.append(number)
-        
         f.close()
+
 
         return training_set, expected_output
 
@@ -78,21 +79,25 @@ def generate_input_with_noise(file_path, ammount_of_inputs):
 
 
 def main():
-    training_set, expected_output = parse_ej2("ex_3/resources/training")
+    training_set, expected_output = parse_ej3("ex_3/resources/training")
     input_dimension = len(training_set[0])
     expected_output_dimension = len(expected_output[0])
+    print("expected output dim: " + str(expected_output[0]))
 
     multilayer_perceptron = MultiLayerPerceptron(learning_rate=0.1, hidden_layers=[2], input_dim=input_dimension, output_dim=expected_output_dimension, update_frequency= 0, activation_function=np.vectorize(lambda x: np.tanh(x)), activation_function_derivative=np.vectorize(lambda x: 1-(np.tanh(x))**2), update_learn_rate=0.1, scale_output=False)
-    multilayer_perceptron.train(training_set, expected_output, epoch_limit=100, callback=(lambda i, error, weights, output: print("Iteration: {}, Error: {}, Output: {}".format(i, error, str(output)))))
-    output = multilayer_perceptron.scale_output()
+    multilayer_perceptron.train(training_set, expected_output, epoch_limit=1000, callback=(lambda i, error, weights, output: print("Iteration: {}, Error: {}".format(i, error, str(output)))))
+    #output = multilayer_perceptron.scale_output()
     #error_vs_iteration("ex_3/resources/xor/training", False)
+    noise_training_set = generate_input_with_noise("ex_3/resources/training", 10)
+    prediction = multilayer_perceptron.forward_propagation(noise_training_set[1])
+    
+    print("the prediction is: " + str(prediction))
 
-    print("output: " + str(output))
+if __name__ == "__main__":
+     main()
 
-#if __name__ == "__main__":
-#     main()
 
-generate_input_with_noise("ex_3/resources/training", 10)
+#generate_input_with_noise("ex_3/resources/training", 10)
 
 #parse_training("ex_3/resources/training")
 
