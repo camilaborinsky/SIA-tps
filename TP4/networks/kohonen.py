b15b39aeff4b5c0b911da0a8d66e324b4cbeea3f
camilaborinsky <cborinsky@itba.edu.ar>
2022-05-26 12:13:22 -0300

    fix kohonen and start with kohonen notebook

--- TP4/networks/kohonen.py
+++ TP4/networks/kohonen.py
@@ -11,8 +11,11 @@ class KohonenNetwork:
         self.output_dimension = output_dimension
         self.initial_radius = output_dimension/2
         self.initial_learning_rate = initial_learning_rate
+        self.learning_rate = initial_learning_rate,
+        self.radius = self.initial_radius
     
     def initialize_grid(self, training_set):
+        self.weights = [[None for l in range(self.output_dimension)] for k in range(self.output_dimension)]
         for row in range(self.output_dimension):
             for col in range(self.output_dimension):
                 idx = np.random.randint(0, len(training_set))
@@ -26,13 +29,15 @@ class KohonenNetwork:
             for j in range(self.output_dimension):
                 dist = self.calculate_distance(self.weights[i][j], _input)
                 if dist < min_distance:
+                    min_distance = dist
                     to_return=[(i,j)]
                 elif dist == min_distance:
                     to_return.append((i,j))
+        # print(to_return)
         return random.choice(to_return)
     
     def calculate_distance(self, input1, input2):
-        return np.linalg.norm(np.subtract(input1 - input2), 2)
+        return np.linalg.norm(np.subtract(input1, input2), 2)
     
     def update_weights(self, i, j, _input):
         for row in range(self.output_dimension):
@@ -41,13 +46,37 @@ class KohonenNetwork:
                     self.weights[row][col] += self.learning_rate * (_input - self.weights[row][col])
                     
 
-    def train(self, training_set, epochs):
-        p = len(training_set)
+    def train(self, training_set, input_ids, epochs):
+        self.p = len(training_set)
         self.initialize_grid(training_set)
+        self.epochs = epochs
+        self.data = list(zip(input_ids, training_set))
+        # print(self.data)
         e = 0
+        k=0
         while e < epochs:
-            random.shuffle(training_set)
-            for _input in training_set:
-                i, j = self.get_activated_neuron(_input)
-                self.update_weights(i, j, _input)
+            random.shuffle(self.data)
+            # print(e)
+            # print(self.data)
+            self.groups = [[list() for l in range(self.output_dimension)] for k in range(self.output_dimension)]
+            for _input in self.data:
+                i, j = self.get_activated_neuron(_input[1])
+                # print(f"{i}, {j}")
+                self.update_weights(i, j, _input[1])
+               
+                self.groups[i][j].append(_input[0])
+                # print(self.groups)
+                # print(self.radius)
+                # print(self.learning_rate)
+
+                self.update_learn_rate(e, k)
+                k+=1
             e+=1
+            self.update_radius(e, k)
+        return e, self.groups
+
+    def update_learn_rate(self, e, k):
+        self.learning_rate = self.initial_learning_rate * (1- (k+1) / ((self.epochs)*self.p))
+    
+    def update_radius(self, e, k):
+        self.radius = self.initial_radius * (1- (k+1) / ((self.epochs)*self.p))
