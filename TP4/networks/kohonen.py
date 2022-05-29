3c0736a9ea0e982ab1d7f268f91afdbd6dee5849
camilaborinsky <cborinsky@itba.edu.ar>
2022-05-29 20:16:57 -0300

    some graphs for kohonen

--- TP4/networks/kohonen.py
+++ TP4/networks/kohonen.py
@@ -11,7 +11,7 @@ class KohonenNetwork:
         self.output_dimension = output_dimension
         self.initial_radius = output_dimension/2
         self.initial_learning_rate = initial_learning_rate
-        self.learning_rate = initial_learning_rate,
+        self.learning_rate = initial_learning_rate
         self.radius = self.initial_radius
     
     def initialize_grid(self, training_set):
@@ -30,10 +30,9 @@ class KohonenNetwork:
                 dist = self.calculate_distance(self.weights[i][j], _input)
                 if dist < min_distance:
                     min_distance = dist
-                    to_return=[(i,j)]
+                    to_return=[(i,j, dist)]
                 elif dist == min_distance:
-                    to_return.append((i,j))
-        # print(to_return)
+                    to_return.append((i,j, dist))
         return random.choice(to_return)
     
     def calculate_distance(self, input1, input2):
@@ -46,37 +45,60 @@ class KohonenNetwork:
                     self.weights[row][col] += self.learning_rate * (_input - self.weights[row][col])
                     
 
-    def train(self, training_set, input_ids, epochs):
+    def train(self, training_set, input_ids, epochs, callback):
         self.p = len(training_set)
         self.initialize_grid(training_set)
         self.epochs = epochs
         self.data = list(zip(input_ids, training_set))
-        # print(self.data)
+        self.radius_values = []
+        self.learning_rate_values = []
         e = 0
         k=0
         while e < epochs:
             random.shuffle(self.data)
-            # print(e)
-            # print(self.data)
-            self.groups = [[list() for l in range(self.output_dimension)] for k in range(self.output_dimension)]
             for _input in self.data:
-                i, j = self.get_activated_neuron(_input[1])
-                # print(f"{i}, {j}")
-                self.update_weights(i, j, _input[1])
-               
-                self.groups[i][j].append(_input[0])
-                # print(self.groups)
-                # print(self.radius)
-                # print(self.learning_rate)
-
+                self.radius_values.append(self.radius)
+                self.learning_rate_values.append(self.learning_rate)
+                i, j, d = self.get_activated_neuron(_input[1])
+                self.update_weights(i, j, _input[1])               
                 self.update_learn_rate(e, k)
                 k+=1
+            callback(e, self.get_quantization_error(training_set))
             e+=1
             self.update_radius(e, k)
-        return e, self.groups
 
     def update_learn_rate(self, e, k):
-        self.learning_rate = self.initial_learning_rate * (1- (k+1) / ((self.epochs)*self.p))
+        self.learning_rate = self.initial_learning_rate*np.exp(-2*k/((self.epochs)*self.p))
+        # self.learning_rate = self.initial_learning_rate * (1- (k) / ((self.epochs)*self.p))
     
     def update_radius(self, e, k):
-        self.radius = self.initial_radius * (1- (k+1) / ((self.epochs)*self.p))
+        self.radius = self.initial_radius*np.exp(-5*k/((self.epochs)*self.p)) + 1
+        # self.radius = self.initial_radius/2 * (2- (k+1) / ((self.epochs)*self.p))
+
+
+    def u_matrix(self):
+        u_matrix = np.zeros((self.output_dimension, self.output_dimension))
+        for row in range(self.output_dimension):
+            for col in range(self.output_dimension):
+                available_dirs = 0
+                if row > 0:
+                    available_dirs +=1
+                    u_matrix[row][col] += self.calculate_distance(self.weights[row][col], self.weights[row-1][col])
+                if row < self.output_dimension-1:
+                    available_dirs +=1
+                    u_matrix[row][col] += self.calculate_distance(self.weights[row][col], self.weights[row+1][col])
+                if col > 0:
+                    available_dirs +=1
+                    u_matrix[row][col] += self.calculate_distance(self.weights[row][col], self.weights[row][col-1])
+                if col < self.output_dimension-1:
+                    available_dirs +=1
+                    u_matrix[row][col] += self.calculate_distance(self.weights[row][col], self.weights[row][col+1])
+                u_matrix[row][col] /= available_dirs             
+        return u_matrix
+    
+    def get_quantization_error(self, training_set):
+        quantization_error = 0
+        for _input in training_set:
+            i, j, d = self.get_activated_neuron(_input)
+            quantization_error += d
+        return quantization_error/len(training_set)
