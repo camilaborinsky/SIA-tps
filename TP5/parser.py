45746cd40c8000eeced7dfaf0c9f2f7c93e801a0
camilaborinsky <cborinsky@itba.edu.ar>
2022-06-16 00:12:26 -0300

    parsing letters and printing

---
+++ TP5/parser.py
@@ -0,0 +1,27 @@
+
+from visualize_letters import print_letter
+
+
+def data_converter(file_path):
+    with open(file_path + "/fonts.txt") as f:
+        characters = list()
+        for current_line in f.readlines():
+            current_line = current_line.strip()
+            current_line = current_line.replace('{', '').replace('}', '')
+            values = []
+            values = current_line.split(',')
+            binaries = []
+            print(str(values))
+            for h in values:
+                bins = bin(int(h, 16))[2:].zfill(5)
+                binaries.append([int(b) for b in bins])
+                # print(str(bins))
+            characters.append(binaries)
+    f.close()
+
+    for letter in characters:
+        print(str(letter))
+        # print_letter(letter)
+
+    print_letter(characters[4])
+data_converter("resources")
---
+++ TP5/visualize_letters.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+
+def parser_letters(path_to_file):
+    print("Enter parser")
+    all_letters = []
+    matrix = []
+    with open(path_to_file) as f:
+        counter = 0
+        for current_line in f: 
+            current_line = current_line.strip()
+            if counter % 5 == 0 and counter != 0:
+                all_letters.append(matrix)
+                matrix = []
+                count = 0
+            
+            counter += 1
+            a,b,c,d,e = current_line.split(",")
+            matrix.append([a,b,c,d,e])
+    f.close()
+    all_letters.append(matrix)
+    print(str(all_letters))
+    return all_letters
+
+
+
+
+def letters_with_asterisks(all_letters_matrix):
+    print(str(all_letters_matrix))
+    for letter in all_letters_matrix:
+        for row in letter:
+            for item in row: 
+                if item == '1':
+                    print(" * ", end="")
+                else:
+                    print("   ", end="")
+            print("\n")
+        print("\n")
+
+
+def print_letter(letter_as_binary_matrix):
+    plt.figure(figsize=(7,7))
+    plt.imshow(letter_as_binary_matrix, cmap='Greys')
+    plt.show()
+
